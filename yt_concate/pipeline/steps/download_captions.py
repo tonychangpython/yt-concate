6913378568374f124593f2410362021dd590b6f6
tonychang <tony31318921@gmail.com>
2021-07-13 05:58:21 +0800

    Successfully download first url caption and save video list to file

---
+++ yt_concate/pipeline/steps/download_captions.py
@@ -0,0 +1,35 @@
+import os
+
+from pytube import YouTube
+
+from yt_concate.pipeline.steps.step import Step
+from .step import StepException
+
+import time
+
+class DownloadCaptions(Step):
+    def process(self, data, inputs, utils):
+
+        start = time.time()
+        for url in data:
+            print('downloading caption for', url)
+            if utils.caption_file_exists(url):
+                print('found existing caption file')
+                continue
+            try:
+                source = YouTube(url)
+                en_caption = source.captions.get_by_language_code('a.en')
+                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+                print(en_caption_convert_to_srt)
+            except (AttributeError, KeyError):
+                print('Error when downloading caption for', url)
+                continue
+
+
+            # save the caption to a file named Output.txt
+            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file.write(en_caption_convert_to_srt)
+            text_file.close()
+            break
+        end = time.time()
+        print('took ', end - start, 'seconds')
---
+++ yt_concate/pipeline/steps/preflight.py
@@ -0,0 +1,7 @@
+from yt_concate.pipeline.steps.step import Step
+
+
+class Preflight(Step):
+    def process(self, data, inputs, utils):
+        print('in Preflight')
+        utils.create_dirs()
